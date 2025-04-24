7ccbd91ed61b76f7f07b2183709fd30593d2f08f
eshel.vardi <eshel.vardi@post.runi.ac.il>
2025-04-24 19:01:02 +0300

    add MovieMapper interface for mapping between Movie and MovieDTO

---
+++ src/main/java/com/att/tdp/popcorn_palace/mapper/MovieMapper.java
@@ -0,0 +1,13 @@
+package com.att.tdp.popcorn_palace.mapper;
+import org.mapstruct.Mapper;
+
+import com.att.tdp.popcorn_palace.dto.MovieDTO;
+import com.att.tdp.popcorn_palace.model.Movie;
+
+
+@Mapper(componentModel = "spring")
+public interface MovieMapper {
+    MovieDTO toDTO(Movie movie);
+
+    Movie fromDTO(MovieDTO dto);
+}
